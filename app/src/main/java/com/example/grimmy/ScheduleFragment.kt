d1b7ee0156bed9da962924f0fed3aee0b77d6add
lay <peacheese17@gmail.com>
2025-01-31 09:18:23 +0900

    [UI #8] ScheduleFragment 상단 프로필 LinearLayout 제거 / 에뮬레이터 실행 시 상단에 여전히 공백 발생

--- app/src/main/java/com/example/grimmy/ScheduleFragment.kt
+++ app/src/main/java/com/example/grimmy/ScheduleFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.grimmy.databinding.FragmentReportBinding
+import com.example.grimmy.databinding.FragmentScheduleBinding
 
 class ScheduleFragment : Fragment() {
+
+    private lateinit var binding: FragmentScheduleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+
+        binding = FragmentScheduleBinding.inflate(inflater,container,false)
+
+        return binding.root
     }
 
 }
