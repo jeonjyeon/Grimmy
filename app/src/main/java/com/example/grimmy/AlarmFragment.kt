0b23455d47bd95d485912060be287773e657766c
lay <peacheese17@gmail.com>
2025-02-03 22:59:22 +0900

    [UI #15] fragment_mypage, fragment_alarm 구현 완료

---
+++ app/src/main/java/com/example/grimmy/AlarmFragment.kt
@@ -0,0 +1,22 @@
+package com.example.grimmy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.grimmy.databinding.FragmentAlarmBinding
+
+class AlarmFragment : Fragment() {
+    private lateinit var binding: FragmentAlarmBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlarmBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
