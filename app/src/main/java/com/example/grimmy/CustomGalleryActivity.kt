1fb033940b606b24b414647ff184574e6e09195f
eastlaw02 <ng524855@gmail.com>
2025-02-07 22:42:10 +0900

    [Feat #20] 갤러리 불러오기 기능 추가

--- app/src/main/java/com/example/grimmy/CustomGalleryActivity.kt
+++ app/src/main/java/com/example/grimmy/CustomGalleryActivity.kt
@@ -26,13 +26,14 @@ import android.Manifest
 import android.content.Intent
 import android.provider.Settings
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import com.example.grimmy.databinding.ItemImageBinding
 
 
 class CustomGalleryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCustomGalleryBinding
-    private val imageUris = mutableListOf<Uri>()
+    private val imageMap = mutableMapOf<String, MutableList<Uri>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +46,9 @@ class CustomGalleryActivity : AppCompatActivity() {
 
     private fun setupUI() {
         binding.galleryCloseBtnIv.setOnClickListener { finish() }
+        binding.galleryToggleLl.setOnClickListener {
+            showFolderSelector(imageMap) // imageMap은 폴더 이름과 URI 리스트의 맵
+        }
     }
 
     private fun checkPermissionsAndLoadImages() {
@@ -66,24 +70,49 @@ class CustomGalleryActivity : AppCompatActivity() {
     }
 
     private fun loadImages() {
+
         val uri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        val projection = arrayOf(MediaStore.Images.Media._ID)
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+        )
         val cursor: Cursor? = contentResolver.query(uri, projection, null, null, null)
+
         cursor?.use {
             val idColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            val bucketColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
             while (it.moveToNext()) {
                 val id = it.getLong(idColumn)
+                val bucketName = it.getString(bucketColumn)
                 val contentUri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id.toString())
-                imageUris.add(contentUri)
+                if (!imageMap.containsKey(bucketName)) {
+                    imageMap[bucketName] = mutableListOf()
+                }
+                imageMap[bucketName]?.add(contentUri)
             }
-            Log.d("GalleryApp", "Loaded ${imageUris.size} images.")
         }
-        if (imageUris.isEmpty()) {
-            Log.w("GalleryApp", "No images found in the device.")
-            Toast.makeText(this, "No images found", Toast.LENGTH_SHORT).show()
-        }
+
+        updateGalleryUI(imageMap)
+    }
+
+    private fun updateGalleryUI(images: Map<String, List<Uri>>) {
+        // 이미지 맵에서 선택된 폴더의 이름을 키로 사용하여 이미지 리스트를 가져옵니다.
+        val selectedFolderImages = images.values.first() // 일단 첫 번째 폴더의 이미지들로 초기화합니다.
+
+        // RecyclerView에 어댑터 설정
         binding.galleryAllImgRv.layoutManager = GridLayoutManager(this, 3)
-        binding.galleryAllImgRv.adapter = ImageAdapter(imageUris)
+        binding.galleryAllImgRv.adapter = ImageAdapter(selectedFolderImages)
+    }
+
+    private fun showFolderSelector(imageMap: Map<String, List<Uri>>) {
+        val folders = imageMap.keys.toTypedArray()
+        AlertDialog.Builder(this)
+            .setTitle("Select a Folder")
+            .setItems(folders) { dialog, which ->
+                val selectedFolder = folders[which]
+                updateGalleryUI(mapOf(selectedFolder to imageMap[selectedFolder]!!))
+            }
+            .show()
     }
 
     inner class ImageAdapter(private val imageUris: List<Uri>) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
@@ -93,7 +122,9 @@ class CustomGalleryActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            Glide.with(holder.itemView.context).load(imageUris[position]).into(holder.imageView)
+            Glide.with(holder.itemView.context)
+                .load(imageUris[position])
+                .into(holder.imageView)
         }
 
         override fun getItemCount() = imageUris.size
