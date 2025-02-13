62d954dde941c45ae9a87c2eced96d03cc9143c9
eastlaw02 <ng524855@gmail.com>
2025-02-13 19:47:25 +0900

    [Feat #37] 데일리 코멘트 api 작성 완료

--- app/src/main/java/com/example/grimmy/Retrofit/RetrofitService.kt
+++ app/src/main/java/com/example/grimmy/Retrofit/RetrofitService.kt
@@ -1,44 +1,65 @@
 package com.example.grimmy.Retrofit
 
+import com.example.grimmy.Retrofit.Request.DailyCommentSaveRequest
 import com.example.grimmy.Retrofit.Request.DailyRecordGetRequest
 import retrofit2.Call
 import com.example.grimmy.Retrofit.Request.DailyRecordSaveRequest
 import com.example.grimmy.Retrofit.Request.TestRecordSaveRequest
+import com.example.grimmy.Retrofit.Response.DailyCommentGetResponse
 import com.example.grimmy.Retrofit.Response.DailyRecordGetResponse
 import com.example.grimmy.Retrofit.Response.MonthlyRecordGetResponse
 import com.example.grimmy.Retrofit.Response.TestRecordGetResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RetrofitService {
+    // 데일리 기록 작성
     @POST("/record/daily/save")
     fun postDailyRecordSave(
         @Body request : DailyRecordSaveRequest
     ):Call<Void>
 
+    // 테스트 기록 작성
     @POST("/record/test/save")
     fun postTestRecordSave(
         @Body request : TestRecordSaveRequest
     ):Call<Void>
 
+    // 데일리 코멘트 작성
+    @POST("/record/daily/comment/{dailyId}/save")
+    fun postDailyCommentSave(
+        @Path("dailyId") dailyId: Int,
+        @Body request: DailyCommentSaveRequest
+    ): Call<Void>
+
+    // 데일리 기록 조회
     @GET("/record/daily/get")
     fun getDailyRecordGet(
         @Query(value = "userId") userId: Int,
         @Query(value = "date") date: String
     ):Call<DailyRecordGetResponse>
 
+    // 테스트 기록 조회
     @GET("/record/test/get")
     fun getTestRecordGet(
         @Query(value = "userId") userId: Int,
         @Query(value = "date") date: String
     ):Call<TestRecordGetResponse>
 
+    // 먼슬리 기록 조회
     @GET("/record/monthly")
     fun getMonthlyRecord(
         @Query(value = "userId") userId: Int,
         @Query(value = "year") year: Int,
         @Query(value = "month") month: Int
     ):Call<List<MonthlyRecordGetResponse>>
+
+    // 데일리 코멘트 조회
+    @GET("/record/daily/comment/{dailyId}")
+    fun getDailyComment(
+        @Path("dailyId") dailyId: Int
+    ): Call<List<DailyCommentGetResponse>>
 }
